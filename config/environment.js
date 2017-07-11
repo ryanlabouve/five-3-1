4c080b81c8c98511d40abdd1c1f632535870ff20
Ryan LaBouve <imryanlabouve@gmail.com>
2017-07-10 23:17:19 -0500

    Upgrades ember, fixes bug, and start to write something

--- config/environment.js
+++ config/environment.js
@@ -1,9 +1,10 @@
 /* eslint-env node */
+'use strict';
 
 module.exports = function(environment) {
-  var ENV = {
+  let ENV = {
     modulePrefix: 'five-3-1',
-    environment: environment,
+    environment,
     rootURL: '/',
     locationType: 'auto',
     EmberENV: {
